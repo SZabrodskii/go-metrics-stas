7722860f860f41fe93263aafbd423501123cce0d
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2025-09-12 20:37:18 -0300

    feat(cmd/server): add HTTP server for metrics collection on port 8080

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/SZabrodskii/go-metrics-stas/internal/handler"
+	"github.com/SZabrodskii/go-metrics-stas/internal/repository"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	storage := repository.NewMemStorage()
+
+	metricsHandler := handler.NewMetricsHandler(storage)
+
+	http.HandleFunc("/update/", metricsHandler.UpdateMetric)
+
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("could not start server: %v\n", err)
+	}
+}
