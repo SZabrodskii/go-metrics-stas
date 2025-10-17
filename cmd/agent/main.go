c3e5c3efc82f77b1cda3fa7f4fa285c967fd1831
SZabrodskii <Stanislas.zabrodskii@gmail.com>
2025-10-17 18:07:36 -0300

    chore(cmd/agent): simplify func main()

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,45 +1,17 @@
 package main
 
 import (
-	"context"
-
 	"github.com/SZabrodskii/go-metrics-stas/internal/agent"
 	"github.com/SZabrodskii/go-metrics-stas/internal/config"
 	"github.com/SZabrodskii/go-metrics-stas/pkg/logging"
 	"go.uber.org/fx"
-	"go.uber.org/zap"
 )
 
 func main() {
 	fx.New(
 		logging.Module,
 		config.ProvideAgentConfig(),
-		fx.Provide(
-			func(cfg *config.AgentConfig, logger *zap.Logger) *agent.Agent {
-				return agent.NewAgent(cfg.ServerAddress, cfg.PollInterval, cfg.ReportInterval, logger)
-			},
-		),
-		fx.Invoke(runAgent),
+		agent.Module,
 	).Run()
 
-}
-
-func runAgent(lc fx.Lifecycle, agent *agent.Agent, logger *zap.Logger) {
-	ctx, cancel := context.WithCancel(context.Background())
-
-	lc.Append(fx.Hook{
-		OnStart: func(context.Context) error {
-			go func() {
-				if err := agent.Run(ctx); err != nil && err != context.Canceled {
-					logger.Fatal("Failed to start agent", zap.Error(err))
-				}
-			}()
-			return nil
-		},
-		OnStop: func(context.Context) error {
-			logger.Info("Agent stopped")
-			cancel()
-			return nil
-		},
-	})
 }
