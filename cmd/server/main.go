b08e36269ff6c92156b71e7d1c3d6a98050b03dc
SZabrodskii <Stanislas.zabrodskii@gmail.com>
2026-02-16 10:59:16 -0300

    feat(go-metrics-stas/cmd/server): refine the main.go file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/SZabrodskii/go-metrics-stas/internal/audit"
 	"github.com/SZabrodskii/go-metrics-stas/internal/config"
 	"github.com/SZabrodskii/go-metrics-stas/internal/config/db"
@@ -14,31 +12,6 @@ import (
 	"github.com/SZabrodskii/go-metrics-stas/pkg/logging"
 	"go.uber.org/fx"
 )
-
-var (
-	buildVersion string
-	buildDate    string
-	buildCommit  string
-)
-
-func printBuildInfo() {
-	version := buildVersion
-	if version == "" {
-		version = "N/A"
-	}
-	date := buildDate
-	if date == "" {
-		date = "N/A"
-	}
-	commit := buildCommit
-	if commit == "" {
-		commit = "N/A"
-	}
-
-	fmt.Printf("Build version: %s\n", version)
-	fmt.Printf("Build date: %s\n", date)
-	fmt.Printf("Build commit: %s\n", commit)
-}
 
 func main() {
 	buildinfo.Print()
